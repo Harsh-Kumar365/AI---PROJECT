faa30a1b9fdb34a4afdea80944ccf3d9759e2e7d
HARSH KUMAR <109980074+Harsh-Kumar365@users.noreply.github.com>
2023-11-06 12:17:41 +0530

    Add files via upload

---
+++ DoWhileExample.java
@@ -0,0 +1,20 @@
+package day3_if_else_switch_loops;
+// do while 
+//when you want block of code to be executed atleast once whether condition is true or false
+public class DoWhileExample {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		
+			
+		 do
+		 {
+		 System.out.println("welcome");
+		 i++;
+		 }
+
+		while( i<10);
+	}
+
+}
